a0257f775877ce5792c85768bfc008adc4a5b341
Mehmet Fatih EŞGÜN <mfatihesgun@outlook.com>
2022-07-26 09:33:09 +0300

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("test");
+            
+            Console.ReadKey();
+        }
+    }
+}
